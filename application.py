171024a63d658d1fc5fec10960f0b5bdd84124ca
cranium44 <adabeechaim@gmail.com>
2019-09-03 20:35:32 +0100

    implemented login route

--- application.py
+++ application.py
@@ -1,16 +1,26 @@
-import pip._vendor.requests
+import requests
 import flask.sessions
 import flask
 import mysql.connector
+
 from cs50 import SQL
+from werkzeug.security import check_password_hash, generate_password_hash
+
+
+
 con = mysql.connector.connect()
 db = SQL("http//:localhost/decaflix")
 
 @app.route('/login')
 def login():
    username = requests.form.get("username")
-   pasword = requests.form.get("password")
-   hashed  = genera
+   password = requests.form.get("password")
+   hashed  = generate_password_hash(password)
+   res = db.execute("select * from databas where username = :username", username)
+   if res[0]['hash'] == hash:
+      session['id'] == res[0]['user_id']
+      return redirect('/')
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
