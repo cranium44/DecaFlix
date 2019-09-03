818419fe419878df7290351462cc490aacae2049
Chisom <iwowochisom@gmail.com>
2019-09-03 14:38:37 +0100

    Add navbar in index.html

--- application.py
+++ application.py
@@ -6,16 +6,19 @@ from cs50 import SQL
 con = mysql.connector.connect()
 db = SQL("http//:localhost/decaflix")
 
+
 @app.route('/login')
 def login():
-   username = requests.form.get("username")
-   pasword = requests.form.get("password")
-   hashed  = genera
+    username = requests.form.get("username")
+    pasword = requests.form.get("password")
+    hashed = genera
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-   pass
+    pass
+
 
 @app.route('/', methods=['GET', 'POST'])
 def method_name():
-   pass
+    pass
